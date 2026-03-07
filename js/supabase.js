42d6914e041ac87edbd73ad41afcefcdfd515380
David Gutierrez <cellzt001@gmail.com>
2026-03-07 00:18:33 -0500

    Importación al CDN de supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,6 @@
-const SUPABASE_URL = "https://qfibxdixndfwvcvmzfqv.supabase.co";
-const SUPABASE_KEY = "sb_publishable_N1kcFVFRtLFw9Ywbjy6nLg_31X-KQOc";
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const SUPABASE_URL = "https://qfibxdixndfwvcvmzfqv.supabase.co"
+const SUPABASE_KEY = "sb_publishable_N1kcFVFRtLFw9Ywbjy6nLg_31X-KQOc"
+
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY)
